46441b91406ad387b0f7b3e6668b93a66a9f00cf
brookemccoy16 <brookemccoy16@gmail.com>
2024-01-23 23:27:23 -0800

    Lab2 done

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-define main():
+def main():
     print("Hello World")
 
-if __name__ == "__main__"
+if __name__ == '__main__':
     main()
-
-git config --global user.email "brookemccoy16@gmail.com"
-git config --global user.name "brookemccoy16"
